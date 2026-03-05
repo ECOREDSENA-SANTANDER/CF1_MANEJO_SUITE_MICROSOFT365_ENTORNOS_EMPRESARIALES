7c468727d4f12071806beb20ce3eb57ed1730a8b
lizmanchego <karinamanchego0416@gmail.com>
2026-03-05 10:28:09 -0500

    [DEL] Material complementario

--- src/config/global.js
+++ src/config/global.js
@@ -156,11 +156,6 @@ export default {
         titulo: 'Glosario',
       },
       {
-        nombreRuta: 'complementario',
-        icono: 'far fa-folder-open',
-        titulo: 'Material complementario',
-      },
-      {
         icono: 'fas fa-book',
         titulo: 'Referencias bibliográficas',
         nombreRuta: 'referencias',
